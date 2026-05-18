3c30c7622048249c569327c51c82790d45c3acfa
tdmy66kzr8-bit <tdmy66kzr8@privaterelay.appleid.com>
2026-05-18 19:17:27 +0200

    Add files via upload

---
+++ pages/pages-index.jsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <>
+      <iframe
+        src="/rekant.html"
+        style={{
+          width: '100%',
+          height: '100vh',
+          border: 'none',
+          display: 'block'
+        }}
+      />
+    </>
+  )
+}
